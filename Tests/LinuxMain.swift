1479f14923c10f0e1e33b49eda7e49946abf0b77
Jack Maloney <git@jmmaloney4.xyz>
2021-09-19 21:22:34 -0500

    Fix LinuxMain.swift

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -3,6 +3,5 @@ import XCTest
 import Base58Tests
 
 var tests = [XCTestCaseEntry]()
-tests += StringTests.allTests()
-tests += ArrayWithUInt8Tests.allTests()
+tests += Base58Tests.allTests()
 XCTMain(tests)
